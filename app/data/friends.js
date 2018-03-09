26aceadd2aa358c5dce6f0c1a27b9e9a45dfc90c
Mary Simmons <MarySimmons@Marys-MacBook-Pro.local>
2018-03-08 21:04:36 -0700

    small error fix

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,31 +4,31 @@ var friendsArray = [
     name: "Jojo",
     photo:
       "https://i.pinimg.com/564x/29/f0/3b/29f03baf35f0cdf28727025ecf00b362.jpg",
-    scores: [5, 3, 3, 2, 1, 1, 1, 2, 3, 2, 5]
+    scores: [5, 3, 3, 2, 1, 1, 1, 2, 3, 2]
   },
   {
     name: "Lauren",
     photo:
       "https://i.pinimg.com/originals/11/8c/2b/118c2bf76a7a322b1a331ccc7976bfa5.jpg",
-    scores: [5, 4, 3, 2, 1, 4, 4, 2, 3, 2, 5]
+    scores: [5, 4, 3, 2, 1, 4, 4, 2, 3, 2]
   },
   {
     name: "Lourdes",
     photo:
       "https://i.pinimg.com/564x/c0/a2/70/c0a2700164da10ddc6035d93e36ffac4.jpg",
-    scores: [5, 5, 3, 2, 3, 3, 5, 2, 3, 3, 5]
+    scores: [5, 5, 3, 2, 3, 3, 5, 2, 3, 3]
   },
   {
     name: "Momma",
     photo:
       "https://i.pinimg.com/564x/29/f0/3b/29f03baf35f0cdf28727025ecf00b362.jpg",
-    scores: [5, 2, 3, 2, 1, 2, 1, 2, 3, 4, 4]
+    scores: [5, 2, 3, 2, 1, 2, 1, 2, 3, 4]
   },
   {
     name: "Meredith",
     photo:
       "http://img-aws.ehowcdn.com/750x428p/s3.amazonaws.com/cuteness_data/s3fs-public/diy_blog/How-Big-Do-Teacup-Piglets-Get.jpg",
-    scores: [5, 1, 3, 2, 1, 3, 1, 2, 3, 1, 5]
+    scores: [5, 1, 3, 2, 1, 3, 1, 2, 3, 1]
   }
 ];
 module.exports = friendsArray;
